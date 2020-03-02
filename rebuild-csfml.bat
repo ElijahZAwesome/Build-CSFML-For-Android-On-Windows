8dbb5b219b1e90444191aa12134b6dba9cb95228
Elijah Bansley <17408285+ElijahZAwesome@users.noreply.github.com>
2020-03-02 16:17:25 -0600

    Oops, was building debug binaries

--- rebuild-csfml.bat
+++ rebuild-csfml.bat
@@ -9,7 +9,7 @@ del Makefile
 del install*
 del cmake*
 
-cmake -DANDROID_ABI=%current_abi% -DANDROID_PLATFORM=android-21 -stdlib=libc++ -DCMAKE_TOOLCHAIN_FILE=%ndk_path%/build/cmake/android.toolchain.cmake -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_NDK=%ndk_path% -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles" ../..
+cmake -DANDROID_ABI=%current_abi% -DANDROID_PLATFORM=android-21 -stdlib=libc++ -DCMAKE_TOOLCHAIN_FILE=%ndk_path%/build/cmake/android.toolchain.cmake -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_NDK=%ndk_path% -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ../..
 %ndk_path%\prebuilt\windows-x86_64\bin\make.exe
 
 del "..\%current_abi%.tmp"
